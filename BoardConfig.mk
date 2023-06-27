4ed26e0accc38b40bfcbc6abddb293858b4a1fb2
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2023-06-27 09:46:42 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/realme/RMX2151
+DEVICE_PATH := device/realme/RMX2001
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
